cc3e5d6de0cc81e75b5534bf8361c87cdefd84d7
Shamwow16 <shamyleg@gmail.com>
2015-10-12 12:01:13 -0500

    Create bio object with properties

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,31 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+/*var name = "Shamyle Ghazali";
+var role = "Front End Web Developer";
+
+
+
+/*$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
+skills = ["communication","critical thinking","teamwork","project management"];
+var bio = {
+	"name":"Shamyle Ghazali",
+	"role":"Front End Web Developer",
+	"contact_info":{
+		email:"shamyleg@gmail.com",
+		mobile:"224-241-4290",
+		skype:"shamyle.ghazali",
+		location:"Chicago" },
+	"picture_URL":"images/biopic.jpg",
+	"welcome_message":"Welcome to Shamyle Ghazali's Resume",
+	"skills":skills
+};
+
+
+
+
+
